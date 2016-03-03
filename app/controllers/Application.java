230e896967af374c202b146c96ac6ec542c5751b
Dylan Hunn <dylhunn@gmail.com>
2016-03-03 14:33:35 -0800

    API endpoint test

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,15 +1,33 @@
 package controllers;
 
 import play.*;
+import play.libs.Json;
 import play.mvc.*;
 import harmony.core.Harmonizer;
 
 import views.html.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Application extends Controller {
 
     public Result index() {
         return ok(index.render("Your new application is ready."));
+    }
+
+    public Result harmonize(String input) {
+        List<String> inputChords = Arrays.asList(input.split("%20"));
+        List<List<String>> result = new ArrayList<>();
+        String str = "";
+        for (String s : inputChords) str = str + s + " ";
+        try {
+            result = Harmonizer.harmonize(str);
+        } catch (Exception e) {
+
+        }
+        return ok(Json.toJson(result));
     }
 
 }
