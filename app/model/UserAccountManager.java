c399b996faeb5733d8536714213cdd7c183ce1e6
Dylan Hunn <dylhunn@gmail.com>
2016-03-11 02:54:56 -0800

    db

--- app/model/UserAccountManager.java
+++ app/model/UserAccountManager.java
@@ -1,11 +1,10 @@
 package model;
 
 import controllers.Application;
+import play.api.mvc.Result;
 import scala.App;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 import play.db.*;
@@ -14,51 +13,57 @@ import javax.sql.DataSource;
 
 public class UserAccountManager {
 
+    // for offline testing purposes
     private static final boolean usemap = false;
     private static Map<String, Application.UserData> testUsers = new HashMap();
 
     public static boolean authenticate(Application.Credentials creds) {
-        if (usemap) if (userRegistered(creds.email)) return testUsers.get(creds.email).password.equals(creds.password);
-        if (usemap) return false;
-
-        return false;
-    }
-
-    public static boolean userRegistered(String email) {
-
-        //try {
-        //    Statement stmt = c.createStatement();
-        //    ResultSet rs = stmt.executeQuery("SELECT a, b, c FROM Table1");
-        //    c.close();
-        //} catch (Exception e) {
-            // TODO
-          //  System.out.println(e.toString());
-        //}
-        if (usemap) return testUsers.keySet().contains(email);
-        if (usemap) return false;
-
-        DataSource ds = DB.getDataSource();
-
-        Connection c = DB.getConnection();
-        try {
-            c.close();
-        } catch (Exception e) {
-            System.out.println(e);
+        if (usemap) {
+            if (userRegistered(creds.email)) return testUsers.get(creds.email).password.equals(creds.password);
+            return false;
         }
 
         return false;
     }
 
+    public static boolean userRegistered(String email) {
+        if (usemap) {
+            return testUsers.keySet().contains(email);
+        }
+
+        DataSource ds = DB.getDataSource();
+
+        Connection c = DB.getConnection();
+        PreparedStatement stmt = null;
+        try {
+            String SQL = "";
+            stmt = c.prepareStatement(SQL);
+            ResultSet rs = stmt.executeQuery();
+            stmt.close();
+            c.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+        return false;
+    }
+
     public static boolean registerUser(Application.UserData data) {
-        if (usemap) if (userRegistered(data.email)) return false;
-        testUsers.put(data.email, data);
-        if (usemap) return true;
+        if (usemap) {
+            if (userRegistered(data.email)) return false;
+            testUsers.put(data.email, data);
+            return true;
+        }
+
 
         return true;
     }
 
     public static Application.UserData getUser(String email) {
-        if (usemap) if (userRegistered(email)) return testUsers.get(email);
+        if (usemap) {
+            if (userRegistered(email)) return testUsers.get(email);
+            return null;
+        }
+
         return null;
     }
 }
